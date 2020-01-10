0de5134e55987f973d06eb9c6bb3af880d55b24a
Chew Teong Han <teonghan@gmail.com>
2020-01-10 11:18:47 +0800

    Update scopus_citations_miner_v2.py

--- scopus_citations_miner_v2.py
+++ scopus_citations_miner_v2.py
@@ -1,10 +1,5 @@
 import csv
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import pandas as pd
 import math
